5664d8bb4493504cca056f4cb738792502de0f14
Devendra Devadiga <devendradevadiga01@gmail.com>
2021-12-24 20:03:40 +0530

    Added prebuilt binaries of OP-TEE 3.15 for QEMU ARMv8

---
+++ optee_qemu_armv8a/run_qemu_share_files.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+nc -z  127.0.0.1 54320 || /usr/bin/gnome-terminal -x ./soc_term.py 54320 &
+nc -z  127.0.0.1 54321 || /usr/bin/gnome-terminal -x ./soc_term.py 54321 &
+while ! nc -z 127.0.0.1 54320 || ! nc -z 127.0.0.1 54321; do sleep 1; done
+
+./qemu-system-aarch64 \
+	-nographic \
+	-serial tcp:localhost:54320 -serial tcp:localhost:54321 \
+	-smp 2 \
+	-s -S -machine virt,secure=on,gic-version=2,virtualization=false \
+	-cpu cortex-a57 \
+	-d unimp -semihosting-config enable=on,target=native \
+	-m 1057 \
+	-bios bl1.bin		\
+	-initrd rootfs.cpio.gz \
+	-kernel Image -no-acpi \
+	-append 'console=ttyAMA0,38400 keep_bootcon root=/dev/vda2 ' \
+	 \
+	-object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0,max-bytes=1024,period=1000 -fsdev local,id=fsdev0,path=./shared_folder,security_model=none -device virtio-9p-device,fsdev=fsdev0,mount_tag=host -netdev user,id=vmnic -device virtio-net-device,netdev=vmnic
